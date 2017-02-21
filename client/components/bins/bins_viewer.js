ad597cc49c42435b57c8aaab7903d2a55b7719bb
eddanieles <ed.danieles@yahoo.com>
2017-02-21 16:45:39 -0500

    added new Meteor method to bins.js

--- client/components/bins/bins_viewer.js
+++ client/components/bins/bins_viewer.js
@@ -3,9 +3,12 @@ import { markdown } from 'markdown';
 
 class BinViewer extends Component {
   render() {
+    const rawHTML = markdown.toHTML(this.props.bin.content);
+
     return (
       <div className="col-xs-4">
         <h5>Output</h5>
+        <div dangerouslySetInnerHTML={{ __html: rawHTML}}></div>
       </div>
     )
   }
